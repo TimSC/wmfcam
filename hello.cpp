5b41ce6866b9c57bf2a1283d4a91b6f289701768
TimSC <t.sheerman-chase@surrey.ac.uk>
2013-10-07 13:42:23 +0100

    Add exceptions

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,6 @@
 
+#include <stdexcept>
+
 #include <boost/python/module.hpp>
 #include <boost/python/def.hpp>
 #include <boost/python.hpp>
@@ -8,14 +10,40 @@
 #include <mfreadwrite.h>
 #include <Mferror.h>
 
+template <class T> void SafeRelease(T **ppT)
+{
+    if (*ppT)
+    {
+        (*ppT)->Release();
+        *ppT = NULL;
+    }
+}
+
 class MediaFoundation
 {
 public:
 	void Init()
 	{
 		HRESULT hr = MFStartup(MF_VERSION);
+		if(!SUCCEEDED(hr))
+			std::runtime_error("Media foundation startup failed");
 
-		CoInitializeEx(NULL, COINIT_MULTITHREADED);
+		hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+		if(!SUCCEEDED(hr))
+			std::runtime_error("CoInitializeEx failed");
+	}
+
+	void ListDevices()
+	{
+		IMFAttributes *pAttributes = NULL;
+		HRESULT hr = MFCreateAttributes(&pAttributes, 1);
+		if(!SUCCEEDED(hr))
+			std::runtime_error("MFCreateAttributes failed");
+
+
+
+
+		SafeRelease(&pAttributes);
 	}
 
 	void DeInit()
