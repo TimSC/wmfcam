278958fe9b34bb9a8354a80cdb48a36a8b833860
TimSC <t.sheerman-chase@surrey.ac.uk>
2013-10-09 15:20:20 +0100

    Decode lumenance

--- wmf.py
+++ wmf.py
@@ -2,6 +2,21 @@
 import wmfbase, time, mjpeg, cStringIO
 from PIL import Image
 
+def DecodeYuy2PixelBuffer(buff, height, width, stride):
+    print len(buff), height, width, stride
+    out = Image.new("RGB", (width, height))
+    outl = out.load()
+    
+    for lineNum in range(height):
+        lineOffset = lineNum * stride
+        for pxPairNum in range(width / 2):
+            y1 = buff[pxPairNum * 4 + lineOffset]
+            y2 = buff[pxPairNum * 4 + lineOffset+2]
+            outl[pxPairNum*2, lineNum] = (y1, y1, y1)
+            outl[pxPairNum*2+1, lineNum] = (y2, y2, y2)
+    
+    return out
+
 class wmfsource(object):
     def __init__(self, mf, deviceId):
         self.friendlyName = "Video source"
@@ -14,7 +29,7 @@ class wmfsource(object):
         frame = self._mf.ProcessSamples(self._deviceId)
         frame['pix'] = None
 
-        #Decode pixels into RGB
+        #Decode MJPEG pixels into RGB
         if 'subtype' in frame and frame['subtype'] == "MFVideoFormat_MJPG":
             jpg = cStringIO.StringIO()
             parseJpeg = mjpeg.ParseJpeg()
@@ -22,6 +37,9 @@ class wmfsource(object):
             jpg.seek(0)
             im = Image.open(jpg)
             frame['pix'] = im
+
+        if 'subtype' in frame and frame['subtype'] == "MFVideoFormat_YUY2":
+            frame['pix'] = DecodeYuy2PixelBuffer(frame['buff'], frame['height'], frame['width'], frame['stride'])
         
         return frame
 
@@ -60,7 +78,7 @@ if __name__ == "__main__":
 
     print cam0.friendlyName
     print "Media types available:", len(cam0.GetMediaTypes())
-    cam0.SetMediaType(31)
+    #cam0.SetMediaType(31)
 
     for j in range(2):
         for i in range(100):
@@ -70,6 +88,9 @@ if __name__ == "__main__":
             if 'width' in frame: print frame['width'],
             if 'height' in frame: print frame['height'],
             print ""
+
+            if 'pix' in frame and frame['pix'] is not None:
+                frame['pix'].save("img{0}.jpg".format(i))
 
         print "Stop camera"
         cam0.Stop()
